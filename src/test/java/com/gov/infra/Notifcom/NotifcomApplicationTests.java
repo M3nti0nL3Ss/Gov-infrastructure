cb66343b45477597b28edef8aed9c77aa1c31a62
M3nti0nl3ss <ayoub-sifo@live.fr>
2023-08-10 21:24:48 +0100

    Spring infra

---
+++ src/test/java/com/gov/infra/Notifcom/NotifcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.infra.Notifcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
